d61d38008cf91a206316c09cc2f4d5d89227ec3b
VickArmand <victormaina1962@gmail.com>
2023-06-07 13:02:53 +0300

    first commit

---
+++ app/src/main/java/com/example/mybasicandroidcalculator/Arithmetics.java
@@ -0,0 +1,16 @@
+package com.example.mybasicandroidcalculator;
+
+public class Arithmetics {
+    public static float addition(float a, float b) {
+        return a + b;
+    }
+    public static float subtraction(float a, float b) {
+        return a - b;
+    }
+    public static float multiplication(float a, float b) {
+        return a * b;
+    }
+    public static float division(float a, float b) {
+        return a / b;
+    }
+}
